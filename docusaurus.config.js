8290814c33f6a51dedbafb7eb4b04d7047dd6c35
lebedev_alex <lebedevalex3@gmail.com>
2023-01-22 23:16:08 +0300

    Trailing slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/logo.ico',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
