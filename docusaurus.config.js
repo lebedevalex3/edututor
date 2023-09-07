dc84da3610015c0f3722726d5d4cf0ab424fba07
lebedev_alex <lebedevalex3@gmail.com>
2023-09-07 18:19:49 +0300

    Test vm

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,10 @@ const config = {
     locales: ['ru'],
   },
   plugins: [
-    ['@docusaurus/plugin-ideal-image', { quality: 70 }],
+    [
+      '@docusaurus/plugin-ideal-image',
+      { quality: 70, max: 600, min: 240, steps: 2 },
+    ],
     [
       'docusaurus-plugin-yandex-metrica',
       {
